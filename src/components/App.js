a72e1437e4b3a36fbb6a1c5f951b42ba2916f5be
Ashish Gupta <guptaashish190@gmail.com>
2019-02-22 23:27:03 +0530

    split init

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
+import Teacher from './Teacher';
+import Student from './Student';
 
 class App extends React.Component {
   render() {
     return (
-        <Router>
-          <div>
-            <Route exact path="/" component={Home} />
-          </div>
-        </Router>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/teacher" component={Teacher} />
+          <Route exact path="/student" component={Student} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/Student/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Student extends React.Component {
+  render() {
+    return (
+      <div>
+            Student
+      </div>
+    );
+  }
+}
+
+export default Student;
