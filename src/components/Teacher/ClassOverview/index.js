de8c79d491e74e3560370e252bc78536f411926b
Ashish Gupta <guptaashish190@gmail.com>
2019-02-22 23:56:11 +0530

    teacher split section

---
+++ src/components/Teacher/ClassOverview/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Overview extends React.Component {
+  render() {
+    return (
+      <div>
+            Overview
+      </div>
+    );
+  }
+}
+
+export default Overview;
