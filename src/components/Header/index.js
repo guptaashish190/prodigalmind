7b78eed5fc002fc6ffc83d89aaf5d29a2eeedebf
TheErrorGuy <lavan.puri99@yahoo.com>
2019-02-23 01:25:10 +0530

    added header style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,17 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header-container">
-            Header
+        <div className="logo-container">
+          <img src="/images/pencil.svg" alt="logo" />
+          <h1>Prodigal<span>Mind</span></h1>
+        </div>
+        <div className="search-bar">
+          <img src="/images/magnifying-glass.svg" alt="search" />
+          <input placeholder="Search" />
+        </div>
+        <div className="control-panel">
+          <h1>hello</h1>
+        </div>
       </div>
     );
   }
