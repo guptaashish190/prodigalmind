a408da4a076b864258f5ac622c1fb4030d9d95fb
TheErrorGuy <lavan.puri99@yahoo.com>
2019-02-24 05:12:44 +0530

    added sidebar content

--- src/components/Student/Sidebar.js
+++ src/components/Student/Sidebar.js
@@ -4,8 +4,11 @@ class Sidebar extends React.Component {
   render() {
     return (
       <div className="sidebar-container">
-        <div>x</div>
-        <div>y</div>
+        <div className="cpanel">
+          <img src="/images/Group 33.svg" alt="home" />
+          <img src="/images/man-user.svg" alt="user" />
+          <img src="/images/ic_timeline_24px.svg" alt="timeline" />
+        </div>
       </div>
     );
   }
