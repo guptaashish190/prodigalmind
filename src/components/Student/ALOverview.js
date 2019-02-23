3384cb05cfd3412fb69983fb621ef3e0a707f8cc
Ashish Gupta <guptaashish190@gmail.com>
2019-02-23 22:12:57 +0530

    added students home init

---
+++ src/components/Student/ALOverview.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ALOverview extends React.Component {
+  render() {
+    return (
+      <div className="aloverview-container">
+        <div className="title">
+                Adaptive Learning Overview
+        </div>
+        <ul className="video-list">
+          <li><img src="images/Rectangle 5.png" alt="recomm" /></li>
+          <li><img src="images/Rectangle 9.png" alt="recomm" /></li>
+          <li><img src="images/Rectangle 20.png" alt="recomm" /></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ALOverview;
--- src/components/Student/index.js
+++ src/components/Student/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import ALOverview from './ALOverview';
+import Sidebar from './Sidebar';
 
 class Student extends React.Component {
+  // Assumptions
+  name = 'Sam Wilson'
+
   render() {
     return (
-      <div>
-            Student
+      <div className="student-section-container">
+        <Sidebar />
+        <div className="main">
+          <div className="greeting">
+              Good Afternoon, {this.name.split(' ')[0]}
+          </div>
+          <ALOverview />
+        </div>
       </div>
     );
   }
