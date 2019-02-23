520d071b39bb1b580a9066b06d65934f41d91901
Ashish Gupta <guptaashish190@gmail.com>
2019-02-24 04:40:55 +0530

    added timeline

--- src/components/Student/Chatroom.js
+++ src/components/Student/Chatroom.js
@@ -4,11 +4,16 @@ class Chatroom extends React.Component {
   render() {
     return (
       <div className="chatroom-container">
-        <img src="" alt="chatroom" />
-        <button>Enter Chatroom</button>
-        <span>
+        <div className="title">
+            Chatroom
+        </div>
+        <div className="main">
+          <img src="/images/group.png" alt="chatroom" />
+          <button>Enter Chatroom</button>
+          <span>
             Give Notifications, messages to students or chat with them.
-        </span>
+          </span>
+        </div>
       </div>
     );
   }
--- src/components/Student/Timeline.js
+++ src/components/Student/Timeline.js
@@ -4,8 +4,37 @@ class Timeline extends React.Component {
   render() {
     return (
       <div className="timeline-container">
-        <div className="title">Timeline</div>
-        <div>y</div>
+        <div className="title">
+            Timeline
+        </div>
+        <div className="main">
+          <ul>
+            <li>
+              <div className="name">
+                    Electrostats quiz
+              </div>
+              <div className="detail">
+                    10 Questions
+              </div>
+            </li>
+            <li>
+              <div className="name">
+                    Electrostatic Potential Test
+              </div>
+              <div className="detail">
+                    14 Questions 100 Marks
+              </div>
+            </li>
+            <li>
+              <div className="name">
+                    Assignment Gravitation
+              </div>
+              <div className="detail">
+                    4 Members Team
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
