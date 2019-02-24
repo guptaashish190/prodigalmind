4579ac8286f37837b88250576a5f64c296d18353
Arpit Jain <arpitkotecha1999@gmail.com>
2019-02-24 14:49:52 +0530

    revert last subtree

--- PM-Backend/PM-Db-Backend/app/routes/sendTopics.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(app,db){
-    app.get('/getTopics',(req,res)=>{
-      list = {"Physics":["NLM","Electric Charges","Gravity"],
-      "History":["Ancient civilisations","Roman empire","Modern history"],
-      "Geography":["Planet earth","India","Agriculture"]}
-
-      res.send(list)  
-  }
-}
