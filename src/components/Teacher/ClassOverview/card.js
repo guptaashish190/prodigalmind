8a39ac08f0a1a620998fce57f65674882e13be8c
TheErrorGuy <lavan.puri99@yahoo.com>
2019-02-24 07:35:42 +0530

    added card anim

--- src/components/Teacher/ClassOverview/card.js
+++ src/components/Teacher/ClassOverview/card.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 import $ from 'jquery';
 
+
 class Card extends React.Component {
     chartdata = {
       labels: ['', '', '', ''],
